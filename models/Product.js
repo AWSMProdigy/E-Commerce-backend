3257e24d6b9df7cd97087227a28f0d1b60dc2edb
AWSMProdigy <StraightAKyle@gmail.com>
2021-10-29 20:57:29 -0400

    WOrking on models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,29 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      default: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // references: 'category',
+      // referencesKey: 'id'
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,24 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // References product model's id
+      references: 'product',
+      referencesKey: 'id'
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // References tag model's id
+      // references: "product_tag",
+      // refernecesKey: "id"
+    }
   },
   {
     sequelize,
